95ee5a1a2dd29e736e61467b681d9b1729dac2c7
Michael Gurevich <michael.gurevich.02@mail.ru>
2023-09-29 17:35:04 +0300

    feat: add styling

--- DrawMe/src/app/pages/home/Draw.tsx
+++ DrawMe/src/app/pages/home/Draw.tsx
@@ -1,5 +1,6 @@
-import { MouseEventHandler, useCallback, useEffect, useRef } from "react"
+import { MouseEventHandler, useCallback } from "react"
 
+import { Helmet } from "react-helmet-async"
 import styled from "styled-components"
 
 import Button from "@/components/Button"
@@ -13,43 +14,7 @@ interface IDrawProps {
 }
 
 export default function Draw({ goBack, image }: IDrawProps) {
-  const canvas = useRef<HTMLCanvasElement>(null)
-
   const handleSaveClick = useImageSave(image)
-
-  useEffect(() => {
-    if (!canvas.current) return
-
-    const { pixels: rawPixels, width, height, maxColorValue, isP6 } = image
-    canvas.current.width = width
-    canvas.current.height = height
-    const pixels = maxColorValue > 255 ? new Uint16Array(rawPixels) : rawPixels // convert to Uint16Array if maxColorValue > 255
-    const norm = 255 / maxColorValue // normalize values to range [0, 255]
-
-    // clamped values are integers in range [0, 255]
-    const clampedArray = new Uint8ClampedArray(width * height * 4)
-
-    // color
-    if (isP6)
-      for (let i = 0; i < width * height; i++) {
-        clampedArray[i * 4] = pixels[i * 3] * norm
-        clampedArray[i * 4 + 1] = pixels[i * 3 + 1] * norm
-        clampedArray[i * 4 + 2] = pixels[i * 3 + 2] * norm
-        clampedArray[i * 4 + 3] = 255
-      }
-    // grayscale
-    else
-      for (let i = 0; i < width * height; i++) {
-        clampedArray[i * 4] = pixels[i] * norm
-        clampedArray[i * 4 + 1] = pixels[i] * norm
-        clampedArray[i * 4 + 2] = pixels[i] * norm
-        clampedArray[i * 4 + 3] = 255
-      }
-
-    const imageData = new ImageData(clampedArray, width, height)
-    const context = canvas.current.getContext("2d")
-    context?.putImageData(imageData, 0, 0)
-  }, [image])
 
   const handleLoadClick = useCallback<MouseEventHandler<HTMLButtonElement>>(
     goBack,
@@ -57,23 +22,31 @@ export default function Draw({ goBack, image }: IDrawProps) {
   )
 
   return (
-    <Wrapper>
-      <canvas ref={canvas} />
-      <ButtonWrapper>
-        <Button data-type="secondary" onClick={handleLoadClick}>
-          Загрузить другое
-        </Button>
-        <Button data-type="primary" onClick={handleSaveClick}>
-          Скачать изображение
-        </Button>
-      </ButtonWrapper>
-    </Wrapper>
+    <>
+      <Helmet>
+        <title>Редактирование изображения</title>
+      </Helmet>
+
+      <Wrapper>
+        <Canvas image={image} />
+        <ButtonWrapper>
+          <Button data-type="secondary" onClick={handleLoadClick}>
+            Загрузить другое
+          </Button>
+          <Button data-type="primary" onClick={handleSaveClick}>
+            Скачать изображение
+          </Button>
+        </ButtonWrapper>
+      </Wrapper>
+    </>
   )
 }
 
 const Wrapper = styled.div`
   position: relative;
-  height: 100vh;
+
+  align-items: center;
+  min-height: 100vh;
   box-sizing: border-box;
   padding: 20px;
 `
--- DrawMe/src/app/utils/hooks/useImageSave.ts
+++ DrawMe/src/app/utils/hooks/useImageSave.ts
@@ -3,32 +3,27 @@ import { MouseEventHandler, useCallback } from "react"
 import { IImage } from "@/utils/types/image"
 
 export default function useImageSave(image: IImage) {
-  const handleClick = useCallback<MouseEventHandler<HTMLElement>>(
-    event => {
-      const encoder = new TextEncoder()
-      const header = encoder.encode(
-        `P${image.isP6 ? "6" : "5"}\n${image.width} ${image.height}\n${
-          image.maxColorValue
-        }\n`
-      )
+  const handleClick = useCallback<MouseEventHandler<HTMLElement>>(() => {
+    const encoder = new TextEncoder()
+    const header = encoder.encode(
+      `P${image.isP6 ? "6" : "5"}\n${image.width} ${image.height}\n${
+        image.maxColorValue
+      }\n`
+    )
 
-      const blob = new Blob([header, image.pixels], {
-        type: image.isP6
-          ? "image/x-portable-pixmap"
-          : "image/x-portable-graymap",
-      })
+    const blob = new Blob([header, image.pixels], {
+      type: image.isP6 ? "image/x-portable-pixmap" : "image/x-portable-graymap",
+    })
 
-      const link = document.createElement("a")
-      link.href = URL.createObjectURL(blob)
-      link.download = `image.${image.isP6 ? "ppm" : "pgm"}`
+    const link = document.createElement("a")
+    link.href = URL.createObjectURL(blob)
+    link.download = `image.${image.isP6 ? "ppm" : "pgm"}`
 
-      link.style.display = "none"
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-    },
-    [image]
-  )
+    link.style.display = "none"
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }, [image])
 
   return handleClick
 }
