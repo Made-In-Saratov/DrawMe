0f02eb13cdcf1f19dee01c034d8efeee0b6dea11
Michael Gurevich <michael.gurevich.02@mail.ru>
2023-10-17 23:06:06 +0300

    feat: add change handlers

--- DrawMe/src/app/pages/home/Gamma/GammaEditor.tsx
+++ DrawMe/src/app/pages/home/Gamma/GammaEditor.tsx
@@ -1,3 +1,5 @@
+import { ChangeEventHandler, useCallback, useState } from "react"
+
 import styled from "styled-components"
 
 import Button from "@/components/Button"
@@ -11,13 +13,33 @@ interface IGammaEditorProps {
 }
 
 export default function GammaEditor({ title, tooltip }: IGammaEditorProps) {
+  const [value, setValue] = useState("0")
+
+  const handleInput = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    ({ target }) => setValue(target.value),
+    []
+  )
+
+  const handleClick = useCallback(() => {
+    console.log(value)
+  }, [value])
+
+  const isButtonDisabled = !value || isNaN(Number(value)) || Number(value) < 0
+
   return (
     <Wrapper>
       <Tooltip>{tooltip}</Tooltip>
 
       <span>{title}</span>
-      <Input placeholder="Введите гамму..." />
-      <Button data-type="primary">Готово</Button>
+      <Input
+        value={value}
+        onChange={handleInput}
+        placeholder="Гамма..."
+        type="number"
+      />
+      <Button data-type="primary" disabled={isButtonDisabled}>
+        Готово
+      </Button>
     </Wrapper>
   )
 }
@@ -35,6 +57,17 @@ const Wrapper = styled.div`
   > span {
     ${text16};
     color: var(--dark-blue);
+  }
+
+  > ${Input} {
+    width: 120px;
+    appearance: textfield;
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
   }
 
   > ${Button} {
