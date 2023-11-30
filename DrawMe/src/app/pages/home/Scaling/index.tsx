0d128884cd73c7e7f84747d848002a95cf20641c
Michael Gurevich <michael.gurevich.02@mail.ru>
2023-11-30 22:42:59 +0300

    feat: add support for applying

--- DrawMe/src/app/pages/home/Scaling/index.tsx
+++ DrawMe/src/app/pages/home/Scaling/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react"
+import { ChangeEventHandler, useCallback, useState } from "react"
 
 import { Helmet } from "react-helmet-async"
 import styled from "styled-components"
@@ -9,12 +9,31 @@ import EditWrapper from "@/components/EditWrapper"
 import Input from "@/components/Input"
 import Line from "@/components/Line"
 import { ScalingAlgorithmT } from "@/pages/home/Scaling/types"
+import { useAppDispatch, useAppSelector } from "@/store"
+import { setPixels } from "@/store/slices/image"
 import { text16 } from "@/utils/fonts"
 import { scaling } from "@/utils/scaling"
 
 export default function Scaling() {
+  const dispatch = useAppDispatch()
+
+  const { src: image } = useAppSelector(({ image }) => image)
+
   const [scalingAlgorithm, setScalingAlgorithm] =
     useState<ScalingAlgorithmT>("NearestNeighbor")
+
+  const [newWidth, setNewWidth] = useState<string>(
+    image?.width.toString() ?? "0"
+  )
+  const [newHeight, setNewHeight] = useState<string>(
+    image?.width.toString() ?? "0"
+  )
+
+  const [offsetX, setOffsetX] = useState<string>("0")
+  const [offsetY, setOffsetY] = useState<string>("0")
+
+  const [b, setB] = useState<string>("0")
+  const [c, setC] = useState<string>("0.5")
 
   const handleItemChange = useCallback((item: string) => {
     for (const [key, value] of Object.entries(scaling))
@@ -23,6 +42,54 @@ export default function Scaling() {
         break
       }
   }, [])
+
+  const handleNewWidthChange = useCallback<
+    ChangeEventHandler<HTMLInputElement>
+  >(({ target }) => setNewWidth(target.value), [])
+
+  const handleNewHeightChange = useCallback<
+    ChangeEventHandler<HTMLInputElement>
+  >(({ target }) => setNewHeight(target.value), [])
+
+  const handleOffsetXChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    ({ target }) => setOffsetX(target.value),
+    []
+  )
+
+  const handleOffsetYChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    ({ target }) => setOffsetY(target.value),
+    []
+  )
+
+  const handleBChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    ({ target }) => setB(target.value),
+    []
+  )
+
+  const handleCChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    ({ target }) => setC(target.value),
+    []
+  )
+
+  const handleApply = useCallback(() => {
+    if (image) {
+      const { pixels, width, height } = image
+      const newPixels = scaling[scalingAlgorithm].apply({
+        pixels,
+        width,
+        height,
+        newWidth: Number(newWidth),
+        newHeight: Number(newHeight),
+      })
+      dispatch(
+        setPixels({
+          pixels: newPixels,
+          width: Number(newWidth),
+          height: Number(newHeight),
+        })
+      )
+    }
+  }, [dispatch, image, newHeight, newWidth, scalingAlgorithm])
 
   return (
     <>
@@ -39,25 +106,25 @@ export default function Scaling() {
 
         <Line />
 
-        <BCInputBlock>
+        <InputBlock>
           <span>Размеры</span>
           <div>
             <Input
               type="number"
-              value={0}
-              onChange={() => {}}
+              value={newWidth}
+              onChange={handleNewWidthChange}
               placeholder="↔"
             />
             ×
             <Input
               type="number"
-              value={0}
-              onChange={() => {}}
+              value={newHeight}
+              onChange={handleNewHeightChange}
               placeholder="↕"
             />
             px
           </div>
-        </BCInputBlock>
+        </InputBlock>
 
         <Line />
 
@@ -66,15 +133,15 @@ export default function Scaling() {
           <div>
             <Input
               type="number"
-              value={0}
-              onChange={() => {}}
+              value={offsetX}
+              onChange={handleOffsetXChange}
               placeholder="→"
             />
             ,
             <Input
               type="number"
-              value={0}
-              onChange={() => {}}
+              value={offsetY}
+              onChange={handleOffsetYChange}
               placeholder="↓"
             />
           </div>
@@ -90,15 +157,15 @@ export default function Scaling() {
                 B:
                 <Input
                   type="number"
-                  value={0}
-                  onChange={() => {}}
+                  value={b}
+                  onChange={handleBChange}
                   placeholder="B"
                 />
                 C:
                 <Input
                   type="number"
-                  value={0}
-                  onChange={() => {}}
+                  value={c}
+                  onChange={handleCChange}
                   placeholder="C"
                 />
               </div>
@@ -106,7 +173,7 @@ export default function Scaling() {
           </>
         )}
 
-        <Button data-type="primary" onClick={() => {}}>
+        <Button data-type="primary" onClick={handleApply}>
           Применить
         </Button>
       </StyledEditWrapper>
