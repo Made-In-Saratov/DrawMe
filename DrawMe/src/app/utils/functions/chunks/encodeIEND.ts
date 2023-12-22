24aebd468a27c74e68223916c153a829ec3f91ae
Michael Gurevich <michael.gurevich.02@mail.ru>
2023-12-22 17:16:50 +0300

    fix: correct `IEND` chunk crc

--- DrawMe/src/app/utils/functions/chunks/encodeIEND.ts
+++ DrawMe/src/app/utils/functions/chunks/encodeIEND.ts
@@ -6,7 +6,7 @@ const encoder = new TextEncoder()
 const name = encoder.encode("IEND")
 
 export default function encodeIEND() {
-  const crc = CRC32.buf(name, CRC32.bstr("IEND"))
+  const crc = CRC32.bstr("IEND")
   const chunkLength = intToBytes(0, 4)
   const crcBytes = intToBytes(crc, 4)
   return new Uint8Array([...chunkLength, ...name, ...crcBytes])
