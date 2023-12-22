62cbe83d7a9d0d31aac8de471b3fa5a098f7e081
Michael Gurevich <michael.gurevich.02@mail.ru>
2023-12-22 16:39:06 +0300

    feat: refactor unfilters

--- DrawMe/src/app/utils/functions/processPNG.ts
+++ DrawMe/src/app/utils/functions/processPNG.ts
@@ -6,6 +6,7 @@ import readgAMA from "@/utils/functions/chunks/readgAMA"
 import readIDAT from "@/utils/functions/chunks/readIDAT"
 import readIHDR from "@/utils/functions/chunks/readIHDR"
 import readPLTE from "@/utils/functions/chunks/readPLTE"
+import unfilters from "@/utils/functions/pngFilters"
 
 const decoder = new TextDecoder("utf-8")
 
@@ -96,7 +97,7 @@ export function processPNG(data: ArrayBuffer): [IImage, number] {
           )
           const filterType = inflatedData[(image.width * bytesPerPixel + 1) * y]
 
-          const unfilterFunc = NumToUnfilter[filterType]
+          const unfilterFunc = unfilters[filterType]
           unfilterFunc(
             scanline,
             pixels,
@@ -137,151 +138,4 @@ function readChunkHeader(byteArray: Uint8Array, offset: number) {
   const chunkLength = bytesToInt(byteArray.slice(offset, offset + 4), 4)
   const chunkType = decoder.decode(byteArray.slice(offset + 4, offset + 8))
   return { chunkLength, chunkType }
-}
-
-const NumToUnfilter: Record<number, typeof unFilterNone> = {
-  0: unFilterNone,
-  1: unFilterSub,
-  2: unFilterUp,
-  3: unFilterAverage,
-  4: unFilterPaeth,
-}
-
-/* eslint-disable no-param-reassign */
-function unFilterNone(
-  scanline: Uint8Array,
-  pixels: number[],
-  bpp: number,
-  offset: number,
-  length: number
-) {
-  for (let i = 0, to = length; i < to; i++) pixels[offset + i] = scanline[i]
-}
-
-function unFilterSub(
-  scanline: Uint8Array,
-  pixels: number[],
-  bpp: number,
-  offset: number,
-  length: number
-) {
-  let i = 0
-  for (; i < bpp; i++) pixels[offset + i] = scanline[i]
-  for (; i < length; i++) {
-    // Raw(x) + Raw(x - bpp)
-    pixels[offset + i] = (scanline[i] + pixels[offset + i - bpp]) & 0xff
-  }
-}
-
-function unFilterUp(
-  scanline: Uint8Array,
-  pixels: number[],
-  bpp: number,
-  offset: number,
-  length: number
-) {
-  let i = 0,
-    byte,
-    prev
-  // Prior(x) is 0 for all x on the first scanline
-  if (offset - length < 0)
-    for (; i < length; i++) {
-      pixels[offset + i] = scanline[i]
-    }
-  else
-    for (; i < length; i++) {
-      // Raw(x)
-      byte = scanline[i]
-      // Prior(x)
-      prev = pixels[offset + i - length]
-      pixels[offset + i] = (byte + prev) & 0xff
-    }
-}
-
-function unFilterAverage(
-  scanline: Uint8Array,
-  pixels: number[],
-  bpp: number,
-  offset: number,
-  length: number
-) {
-  let i = 0,
-    byte,
-    prev,
-    prior
-  if (offset === 0) {
-    // Prior(x) == 0 && Raw(x - bpp) == 0
-    for (; i < bpp; i++) {
-      pixels[offset + i] = scanline[i]
-    }
-    // Prior(x) == 0 && Raw(x - bpp) != 0 (right shift, prevent doubles)
-    for (; i < length; i++) {
-      pixels[offset + i] =
-        (scanline[i] + (pixels[offset + i - bpp] >> 1)) & 0xff
-    }
-  } else {
-    // Prior(x) != 0 && Raw(x - bpp) == 0
-    for (; i < bpp; i++) {
-      pixels[offset + i] =
-        (scanline[i] + (pixels[offset - length + i] >> 1)) & 0xff
-    }
-    // Prior(x) != 0 && Raw(x - bpp) != 0
-    for (; i < length; i++) {
-      byte = scanline[i]
-      prev = pixels[offset + i - bpp]
-      prior = pixels[offset + i - length]
-      pixels[offset + i] = (byte + ((prev + prior) >> 1)) & 0xff
-    }
-  }
-}
-
-function unFilterPaeth(
-  scanline: Uint8Array,
-  pixels: number[],
-  bpp: number,
-  offset: number,
-  length: number
-) {
-  let i = 0,
-    raw,
-    a,
-    b,
-    c,
-    p,
-    pa,
-    pb,
-    pc,
-    pr
-  if (offset - length < 0) {
-    // Prior(x) == 0 && Raw(x - bpp) == 0
-    for (; i < bpp; i++) {
-      pixels[offset + i] = scanline[i]
-    }
-    // Prior(x) == 0 && Raw(x - bpp) != 0
-    // paethPredictor(x, 0, 0) is always x
-    for (; i < length; i++) {
-      pixels[offset + i] = (scanline[i] + pixels[offset + i - bpp]) & 0xff
-    }
-  } else {
-    // Prior(x) != 0 && Raw(x - bpp) == 0
-    // paethPredictor(x, 0, 0) is always x
-    for (; i < bpp; i++) {
-      pixels[offset + i] = (scanline[i] + pixels[offset + i - length]) & 0xff
-    }
-    // Prior(x) != 0 && Raw(x - bpp) != 0
-    for (; i < length; i++) {
-      raw = scanline[i]
-      a = pixels[offset + i - bpp]
-      b = pixels[offset + i - length]
-      c = pixels[offset + i - length - bpp]
-      p = a + b - c
-      pa = Math.abs(p - a)
-      pb = Math.abs(p - b)
-      pc = Math.abs(p - c)
-      if (pa <= pb && pa <= pc) pr = a
-      else if (pb <= pc) pr = b
-      else pr = c
-      pixels[offset + i] = (raw + pr) & 0xff
-    }
-  }
 }
